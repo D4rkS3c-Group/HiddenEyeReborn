c66912e4ebae633192955aece22cb3315ab05d72
sTiKyt <stikyt@protonmail.com>
2020-09-21 19:53:30 +0000

    Started to fill-in setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+__author__ = "Open Security Group"
+__version__ = "0.0a1.dev1"
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="hidden-eye-reborn-OSG",
+    version=__version__,
+    author=__author__,
+    description="I'll write that later",
+    python_requires=">=3.6",  # TODO replace with script
+    url="https://github.com/Open-Security-Group-OSG/HiddenEyeReborn",
+    license="The Unlicense",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers="",
+)
