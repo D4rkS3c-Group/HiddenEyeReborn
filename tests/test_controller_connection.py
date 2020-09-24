b8b901b56634e83ed376eab219e1d12bd1aed7ec
sTiKyt <stikyt@protonmail.com>
2020-09-24 17:23:42 +0000

    Added test for verify_connection method

---
+++ tests/test_controller_connection.py
@@ -0,0 +1,13 @@
+import unittest
+from HE_RE.controllers.controller_connection import ConnectionController
+
+
+class ConnectionControllerTests(unittest.TestCase):
+    def test_verify_connection(self):
+        self.assertTrue(ConnectionController(host="https://google.com").verify_connection())
+        self.assertTrue(ConnectionController(host="https://firefox.com").verify_connection())
+        self.assertTrue(ConnectionController(host="https://youtube.com").verify_connection())
+
+
+if __name__ == '__main__':
+    unittest.main()
