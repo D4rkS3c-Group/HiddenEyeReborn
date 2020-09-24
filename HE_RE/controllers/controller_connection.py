b8442e17a6adf3580194885b368afc7730933c2d
sTiKyt <stikyt@protonmail.com>
2020-09-24 17:23:25 +0000

    Added check that sets self._host to host from __init__ if not None

--- HE_RE/controllers/controller_connection.py
+++ HE_RE/controllers/controller_connection.py
@@ -5,7 +5,7 @@ import urllib.request
 class ConnectionController:
     def __init__(self, host, model=ConnectionModel()):
         self._model = model
-        self._host = model.host
+        self._host = host if not None else self._model.host
 
     def verify_connection(self):
         return True if urllib.request.urlopen(url=self._host).getcode() == 200 else False
