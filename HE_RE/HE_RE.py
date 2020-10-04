dd6997d61f64af8a0c23d4fa37ca913f70fdabcf
sTiKyt <stikyt@protonmail.com>
2020-10-04 18:44:02 +0000

    Created def main() as main entry to HiddenEye: Reborn

--- HE_RE/HE_RE.py
+++ HE_RE/HE_RE.py
@@ -1,4 +1,7 @@
 
+def main():
+    print("Success, you can access this via terminal!")
+    print("Hey, if you can - contact that lazy developer and make him code! Would be glad if you contribute, as well...")
 
 if __name__ in "__main__":
     print("Dude, you gotta code this thing! Stop procrastinating you lazy bastard!")
