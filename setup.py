a89459bd682701ff4eaf13df2ae0f1c1daaa0948
sTiKyt <stikyt@protonmail.com>
2020-10-05 20:33:46 +0000

    Fixed redefined-builtin (W0622) error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # META DATA
 __author__ = "Open Security Group"
 __version__ = "0.0a1.dev2"
-__name__ = "hiddeneye_reborn"
+__package_name__ = "hiddeneye_reborn"
 __description__ = "Appropriate Phishing Tool"
 __python_requires__ = ">=3.6"
 __install_requires__ = ['pywebcopy', ]
