0ff73c0c8f9957e0777e6175c24b147066fe01b4
sTiKyt <stikyt@protonmail.com>
2020-10-06 11:45:22 +0000

    Fixed #16

--- HE_RE/models/model_connection.py
+++ HE_RE/models/model_connection.py
@@ -1,11 +1,26 @@
 class ConnectionModel:
+    """Provides default values for `ConnectionController()`
+
+    """
     def __init__(self, host: str = "https://google.com"):
+        """
+        Args:
+            host: Default URL for connection checking needs.
+        """
         self._host = host
 
     @property
     def host(self):
+        """
+        Returns:
+            str: returns value of host.
+        """
         return self._host
 
     @host.setter
     def host(self, new_host: str):
+        """
+        Args:
+            new_host: New URL to set as host.
+        """
         self._host = new_host
