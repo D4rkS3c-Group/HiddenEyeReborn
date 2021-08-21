584ff5460fa172b2dad9c905c5c59e9c05ab038f
sTiKyt <stikyt@protonmail.com>
2021-08-21 23:50:55 +0300

    Moved if statement to right location

--- hiddeneye_reborn/network/verification.py
+++ hiddeneye_reborn/network/verification.py
@@ -4,6 +4,7 @@ from urllib.error import HTTPError, URLError
 import urllib.request
 import logging
 
+
 def verify_connection(url: str=None, timeout: float=None):
     """Used to verify internet connection.
 
@@ -17,6 +18,9 @@ def verify_connection(url: str=None, timeout: float=None):
     verification_timeout = timeout if timeout is not None else 10
     try:
         url_code = urllib.request.urlopen(url=verification_url, timeout=float(verification_timeout)).getcode()
+        if url_code == 200:
+            logging.info("Verified successfully!")
+            return True
     except(HTTPError, URLError) as error:
         logging.warning('Unable to verify connection with %s due to %s, returning False', verification_url, error)
         return False
@@ -24,9 +28,6 @@ def verify_connection(url: str=None, timeout: float=None):
         logging.warning('Connection to %s timed out, returing False', verification_url )
     else:
         logging.info('Connected, checking code...')
-        if url_code == 200:
-            logging.info("Verified successfully!")
-            return True
         else:
             logging.warning('Wrong code, returning False')
 
