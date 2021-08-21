be9b36d733502bb789aa1061d0d9151ea40f6460
sTiKyt <stikyt@protonmail.com>
2021-08-21 23:21:59 +0300

    Added network verification to hiddeneye_reborn.py

--- hiddeneye_reborn/hiddeneye_reborn.py
+++ hiddeneye_reborn/hiddeneye_reborn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from app_logging.default_logging import logging, log, set_logging_config
+from network.verification import verify_connection
 
 
 import argparse
@@ -17,6 +18,7 @@ def main():
     print("Hey, if you can - contact that lazy developer and make him code! Would be glad if you contribute, as well...")
     set_logging_config(level=LOGGING_LEVEL)
     log.debug("Logging level set to %s", LOGGING_LEVEL)
+    verify_connection()
 
 
 if __name__ == "__main__":
