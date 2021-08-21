834d6e1ce8d9cafbdfb783bcc6bb9c1b9d151336
sTiKyt <stikyt@protonmail.com>
2021-08-21 23:21:26 +0300

    Added logging to hiddeneye_reborn.py

--- hiddeneye_reborn/hiddeneye_reborn.py
+++ hiddeneye_reborn/hiddeneye_reborn.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+from app_logging.default_logging import logging, log, set_logging_config
+
 
 import argparse
 
 
-def initialize_arguments():
+LOGGING_LEVEL = logging.DEBUG
+
+def initialize_app():
     parser = argparse.ArgumentParser()
     parser.parse_args()
 
@@ -11,6 +15,8 @@ def initialize_arguments():
 def main():
     print("Success, you can access this via terminal!")
     print("Hey, if you can - contact that lazy developer and make him code! Would be glad if you contribute, as well...")
+    set_logging_config(level=LOGGING_LEVEL)
+    log.debug("Logging level set to %s", LOGGING_LEVEL)
 
 
 if __name__ == "__main__":
