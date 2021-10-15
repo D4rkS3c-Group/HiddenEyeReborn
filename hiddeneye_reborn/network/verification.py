1e0908712c59ee3ac50676a9535cb4a584adba72
sTiKyt <stikyt@protonmail.com>
2021-10-15 04:15:26 +0300

    Fixed network verification message

--- hiddeneye_reborn/network/verification.py
+++ hiddeneye_reborn/network/verification.py
@@ -20,7 +20,7 @@ def verify_connection(url: str=None, timeout: float=None):
         url_code = urllib.request.urlopen(url=verification_url, timeout=float(verification_timeout)).getcode()
         logging.info('Connected, checking code...')
         if url_code == 200:
-            logging.info("Verified successfully!")
+            logging.info("Network verified successfully!")
             return True
     except(HTTPError, URLError) as error:
         logging.warning('Unable to verify connection with %s due to %s, returning False', verification_url, error)
