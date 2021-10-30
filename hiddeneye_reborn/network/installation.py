5d60a4b30d542fde32a97513a56a454a828d5e09
sTiKyt <stikyt@protonmail.com>
2021-10-30 14:07:23 +0300

    Replaced stdout.read with communicate to avoid deadlocks due to any of the other OS pipe buffers filling up and blocking the child process

--- hiddeneye_reborn/network/installation.py
+++ hiddeneye_reborn/network/installation.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE, DEVNULL
 
 def check_dependency(name: str, command: str = 'which'):
     try:
-        dependency_state = Popen([f"{command}", f"{name}"], stdout=PIPE, stderr=DEVNULL).stdout.read().decode('utf-8')
+        dependency_state = Popen([f"{command}", f"{name}"], stdout=PIPE, stderr=DEVNULL).communicate()[0].decode('utf-8')
     except ValueError():
         dependency_state = None
 
