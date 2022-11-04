ce67bed5256d3996a7d0d63170e2f535662728ca
sTiKyt <stikyt@proton.me>
2022-11-04 15:12:07 +0200

    Updated some pypi classifiers

--- setup.py
+++ setup.py
@@ -52,5 +52,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
