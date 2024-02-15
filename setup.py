151ebf0a68c9c4458eee65ca4eed58d8b06c2edc
sTiKyt <35602029+sTiKyt@users.noreply.github.com>
2024-02-15 22:36:25 +0200

    Update setup metadata and relax dependency versions
    The setup metadata in setup.py has been refactored into a dictionary to improve readability and maintainability. The requirements.txt no longer uses fixed versions for dependencies, instead indicating a minimum required version, to decrease potential conflicts. Additionally, the project versioning in __init__.py has been incremented.

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
 from setuptools import setup, find_packages
+from hiddeneye_reborn import __version__
 
-import hiddeneye_reborn
-
-# META DATA
-__author__ = "Open Security Group"
-# TODO add author email to get rid of warning
-__version__ = hiddeneye_reborn.__version__
-__package_name__ = "hiddeneye_reborn"
-__description__ = "Appropriate Phishing Tool"
-__python_requires__ = ">=3.6.*"
-__install_requires__ = ['pywebcopy', 'pyyaml', 'rich']
-__data_files__ = [("", ["LICENSE"])]
-__url__ = "https://github.com/Open-Security-Group-OSG/HiddenEyeReborn"
+metadata = {
+    "__author__": "Open Security Group",
+    "__version__": __version__,
+    "__package_name__": "hiddeneye_reborn",
+    "__description__": "Appropriate Phishing Tool",
+    "__python_requires__": ">=3.6.*",
+    "__install_requires__": ['pywebcopy', 'pyyaml', 'rich'],
+    "__data_files__": [("", ["LICENSE"])],
+    "__url__": "https://github.com/Open-Security-Group-OSG/HiddenEyeReborn",
+}
 
 with open("README.md", "r") as readme:
-    __long_description__ = readme.read()
-
+    metadata["__long_description__"] = readme.read()
 
 setup(
-    name=__package_name__,
-    version=__version__,
-    author=__author__,
-    description=__description__,
-    python_requires=__python_requires__,
-    install_requires=__install_requires__,
-    url=__url__,
-    long_description=__long_description__,
+    name=metadata["__package_name__"],
+    version=metadata["__version__"],
+    author=metadata["__author__"],
+    description=metadata["__description__"],
+    python_requires=metadata["__python_requires__"],
+    install_requires=metadata["__install_requires__"],
+    url=metadata["__url__"],
+    long_description=metadata["__long_description__"],
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
-    data_files=__data_files__,
+    data_files=metadata["__data_files__"],
     entry_points={
         "console_scripts": [
             'hiddeneye-reborn = hiddeneye_reborn.hiddeneye:execute',
